4ee9719bf0c5ed8cd31232fd72383fa0d16f5405
kdb621 <liradb2000@live.co.kr>
2023-06-28 13:27:45 +0900

    Support optianalMemberExpression types;

--- src/lib/isReplaceableIdentifier.js
+++ src/lib/isReplaceableIdentifier.js
@@ -3,6 +3,7 @@ export default function isReplaceableIdentifier(path, identifiers) {
 		identifiers.hasOwnProperty(path.node.name) &&
 		!path.scope.hasBinding(path.node.name) &&
 		!path.parentPath.isMemberExpression({ property: path.node }) &&
+		!path.parentPath.isOptionalMemberExpression({ property: path.node }) &&
 		!path.parentPath.isFunctionParent() &&
 		!path.parentPath.isProperty() &&
 		!path.parentPath.isModuleSpecifier()
